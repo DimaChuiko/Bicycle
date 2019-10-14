eed8b00dbee50ee1e86e2ca11d714af23e273735
dimachuiko <dimachuiko@gmail.com>
2019-10-14 16:14:04 +0300

    new file

---
+++ js/main.js
@@ -0,0 +1,10 @@
+  // импортируем jQuery
+  //= ../../../node_modules/jquery/dist/jquery.js
+  
+  // импортируем Popper
+  //= ../../../node_modules/popper.js/dist/umd/popper.js
+
+////////////импортируем необходимые js-файлы Bootstrap 4
+//= carousel.js
+/////////////импортируем свои js-файлы
+//= my.js
